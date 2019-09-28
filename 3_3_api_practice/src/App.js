6ff08444f67482c8ffb9cba5b3d74699148088ca
jch <ico1828@gmail.com>
2019-09-28 19:28:44 +0900

    new api를 활용하여 axios.get으로 데이터 가져오기

---
+++ 3_3_api_practice/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import NewsList from './components/NewsList';
+
+class App extends Component {
+	render() {
+		return (
+			<>
+				<NewsList />
+			</>
+		);
+	}
+}
+
+export default App;
