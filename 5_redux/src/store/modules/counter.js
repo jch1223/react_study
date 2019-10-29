94a1f9fff1e83618bd5abdcb33cf4f74a9919b32
jch1223 <ico1828@gmail.com>
2019-10-29 19:08:10 +0900

    counter action

---
+++ 5_redux/src/store/modules/counter.js
@@ -0,0 +1,9 @@
+// action
+const INCREMENT = 'counter/INCREMENT';
+const DECREMENT = 'counter/DECREMENT';
+const CHANGE_COLOR = 'counter/CHANGE_COLOR';
+
+// action creator
+export const increment = () => {
+  return { type: INCREMENT };
+};
