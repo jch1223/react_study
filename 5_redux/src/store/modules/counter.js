6065901acf1302c7500bb9af0744405bc08156bd
jch1223 <ico1828@gmail.com>
2019-10-30 13:50:20 +0900

    counter 액션, 리듀서 생성 및 리덕스 적용

--- 5_redux/src/store/modules/counter.js
+++ 5_redux/src/store/modules/counter.js
@@ -7,3 +7,40 @@ const CHANGE_COLOR = 'counter/CHANGE_COLOR';
 export const increment = () => {
   return { type: INCREMENT };
 };
+export const decrement = () => {
+  return { type: DECREMENT };
+};
+export const changeColor = color => {
+  return { type: CHANGE_COLOR, color };
+};
+
+const initialState = {
+  number: 0,
+  color: '#bfcd7e'
+};
+
+// counter reducer
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        number: state.num + 1
+      };
+
+    case DECREMENT:
+      return {
+        ...state,
+        number: state.num - 1
+      };
+
+    case CHANGE_COLOR:
+      return {
+        ...state,
+        color: action.color
+      };
+
+    default:
+      return state;
+  }
+};
